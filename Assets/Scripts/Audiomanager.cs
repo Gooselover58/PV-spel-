328fbc425fb5e546e26cafb892e976339dbecb19
CptGithubber <axel.nyblad@elev.ga.lbs.se>
2024-01-11 14:07:32 +0100

    Ljud

---
+++ Assets/Scripts/Audiomanager.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Audiomanager : MonoBehaviour
+{
+    public static Audiomanager Instance;
+    public Sound[] musicsounds, sfxsounds;
+    public AudioSource musicsource, sfxsource;
+
+    private void Awake()
+    {
+        if (Instance==null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    private void Start()
+    {
+        PlayMusic("banger");
+    }
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(musicsounds, x => x.name == name);
+        if (s==null)
+        {
+            Debug.Log("Sound not found");
+        }
+        else
+        {
+            musicsource.clip = s.clip;
+            musicsource.Play();
+        }
+    }
+    public void PlaySFX(string name)
+    {
+        Sound s = Array.Find(sfxsounds, x => x.name == name);
+        if (s == null)
+        {
+         Debug.Log("Sound not found");
+        }
+        else
+        {
+            sfxsource.PlayOneShot(s.clip);
+        }
+    }
+    public void Togglemusic()
+    {
+        musicsource.mute = !musicsource.mute;
+
+    }
+    public void Togglesfx()
+    {
+        sfxsource.mute = !sfxsource.mute;
+    }
+    public void Musicvolume(float Music)
+    {
+        musicsource.volume = Music;
+    }
+    public void SFXvolume(float sfx)
+    {
+        sfxsource.volume = sfx;
+    }
+
+}
---
+++ Assets/Scripts/volume.cs
@@ -0,0 +1,11 @@
+
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class volume : MonoBehaviour
+{
+    [SerializeField] private AudioMixer mymix;
+    [SerializeField] private Slider musicslider;
+
+}
