d6ce7ded3270ba9abb77fb263cd3aceb12765dae
Gooselover58 <alexander.ejeblad@elev.ga.lbs.se>
2024-01-12 16:31:28 +0100

    GJORDE SAK MED PIVOTSCRIPT
    Everybody wants to rule the world - Tears for Fears

--- Assets/Scripts/PivotScript.cs
+++ Assets/Scripts/PivotScript.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PivotScript : MonoBehaviour
 {
     [SerializeField] Camera cam;
-    [SerializeField] Vector3 offset;
-    private MovmentScript ms;
+    [SerializeField] List<Vector3> offset;
+    [SerializeField] MovmentScript ms;
     private GameObject user;
     private Rigidbody2D rb;
     public float angle;
@@ -14,10 +14,6 @@ public class PivotScript : MonoBehaviour
     void Start()
     {
         user = transform.parent.gameObject;
-        if (ms.GetComponentInParent<MovmentScript>() != null)
-        {
-            ms = GetComponentInParent<MovmentScript>();
-        }
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -26,7 +22,22 @@ public class PivotScript : MonoBehaviour
         Vector2 mouse = cam.ScreenToWorldPoint(Input.mousePosition);
         Vector2 dir = mouse - rb.position;
         angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.position = user.transform.position + offset;
         rb.rotation = angle;
+        if (ms.y <= 0)
+        {
+            transform.position = user.transform.position + offset[0];
+        }
+        else if (ms.y == 1)
+        {
+            transform.position = user.transform.position + offset[1];
+        }
+        if (ms.x <= 0)
+        {
+            transform.position = user.transform.position + offset[2];
+        }
+        else if (ms.x == 1)
+        {
+            transform.position = user.transform.position + offset[3];
+        }
     }
 }
