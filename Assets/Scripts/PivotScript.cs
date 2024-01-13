447664742819ccce96caefb6bbf6a88213fdf5d6
Gooselover58 <alexander.ejeblad@elev.ga.lbs.se>
2024-01-13 13:28:03 +0100

    Gjorde klart alla rum för nivå 1

--- Assets/Scripts/PivotScript.cs
+++ Assets/Scripts/PivotScript.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class PivotScript : MonoBehaviour
 {
     [SerializeField] Camera cam;
-    [SerializeField] List<Vector3> offset;
-    [SerializeField] MovmentScript ms;
+    [SerializeField] Vector3 offset;
     private GameObject user;
     private Rigidbody2D rb;
     public float angle;
@@ -22,22 +21,7 @@ public class PivotScript : MonoBehaviour
         Vector2 mouse = cam.ScreenToWorldPoint(Input.mousePosition);
         Vector2 dir = mouse - rb.position;
         angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.position = user.transform.position + offset;
         rb.rotation = angle;
-        if (ms.y <= 0)
-        {
-            transform.position = user.transform.position + offset[0];
-        }
-        else if (ms.y == 1)
-        {
-            transform.position = user.transform.position + offset[1];
-        }
-        if (ms.x <= 0)
-        {
-            transform.position = user.transform.position + offset[2];
-        }
-        else if (ms.x == 1)
-        {
-            transform.position = user.transform.position + offset[3];
-        }
     }
 }
