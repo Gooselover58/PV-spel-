e80bec19f82b2de8df041b9789fbe3eb7347049f
Gooselover58 <alexander.ejeblad@elev.ga.lbs.se>
2024-01-15 22:22:43 +0100

    Added item type of scriptableobjects

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+public class Item : ScriptableObject
+{
+    int increase;
+}
+
+public enum itemType
+{
+    Buff, OneTimeUse, Instant
+}
