bf96e99209e6eb6b81ae7c72f66d7067fd68f983
DrDagErr <151174223+DrDagErr@users.noreply.github.com>
2024-01-17 14:10:50 +0100

    Ljud när Enemy Dör och tar skada samt parry ljudet.

--- Assets/Scripts/PlayerControllerScript.cs
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -18,7 +18,8 @@ public class MovmentScript : MonoBehaviour
     public float MovmentSpeed;
     [SerializeField] float RollSpeed;
     [SerializeField] Animator anim;
-    [SerializeField] AudioSource spring; 
+    [SerializeField] AudioSource spring;
+    [SerializeField] AudioSource parry; 
     public float x;
     public float y; 
 
@@ -71,6 +72,9 @@ public class MovmentScript : MonoBehaviour
                     StopCoroutine("ParryCool");
                     canParry = true;
                     Destroy(col.gameObject);
+
+                    parry.Play(); 
+
                 }
             }
         }
